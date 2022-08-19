98a94f8c2ec9603592b9fff00c53295c4f2a11df
ramgopalvarma.92@gmail.com <ramgopalvarma.92@gmail.com>
2022-08-19 12:22:04 +0530

    update color

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,28 @@
 echo HELLO VANI RGV Varma
+
+#Multi line printing
+#syntax: echo -e "Line\nLine2"
+
+## -e option is needed for \n to work
+## double quotes or single quotes is need for \n to work.
+
+## \n ->print a new line
+
+
+
+echo -e "Hello\nWorld"
+
+#printing with colours
+
+#color codes
+#31 RED
+#32 GREEN
+#33 YELLOW
+#34 BLUE
+#35 MAGENTA
+#36 CYAN
+
+# Syntax : echo -e "\e[COLmMESSAGE\e[0m"
+echo -e "\e[31mRed Text\e[0m"
+echo Normal Text
+

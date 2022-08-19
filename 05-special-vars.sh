4595de758f3d21dbe3cc41a9a8468363df4868a1
ramgopalvarma.92@gmail.com <ramgopalvarma.92@gmail.com>
2022-08-19 14:06:29 +0530

    new file

---
+++ 05-special-vars.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Specail variables are $0-$n , $*, $@, $#
+
+echo Script Name = $0
+
+echo First Argument = $1
+echo Second Argument = $2
+
+echo All arguments = $*
+
+echo No of Arguments = $#
+
